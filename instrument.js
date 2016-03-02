1d45723e21d25ac807406602eeff2c56b1dac0ab
Mathieu Rene <mathieu.rene@intel.com>
2016-03-01 19:44:11 -0500

    Initial

---
+++ instrument.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var istanbul = require('istanbul');
+var fs = require('fs');
+
+
+var instrumenter = new istanbul.Instrumenter({
+	coverageVariable: '__coverage__',
+	preserveComments: true
+});
+
+
+var fileName = process.argv[2];
+var outputFilename = process.argv[3];
+
+var saneFileName = fileName.replace('/', '_');
+
+var source = fs.readFileSync(fileName, 'utf8');
+source = instrumenter.instrumentSync(source);
+
+var postamble = `
+process.on('beforeExit', () => {
+	require('fs').writeFileSync('coverage-${saneFileName}.json', JSON.stringify(__coverage__), 'utf8');
+});
+`;
+
+fs.writeFileSync(outputFilename, source + `\n\n${postamble}`, 'utf8');
+
